b483ecdd130140cd403d054a6e84a6b85aa02337
Marcos A. Schuelter <marcos.schuelter@amcom.com.br>
2020-01-07 07:43:44 -0300

    #9614 Criação de processo para importação ds resultados da ProvaSP 2019

---
+++ Scripts/1.30.5.0/01_ProvaSP_ResultadosProvaSP2019.sql
@@ -0,0 +1,11 @@
+INSERT INTO ProvaSP.dbo.FatorAssociadoQuestionario(FatorAssociadoQuestionarioId, Edicao, Nome)
+	VALUES(5, '2019', 'Estudante');
+
+INSERT INTO ProvaSP.dbo.FatorAssociadoQuestionario(FatorAssociadoQuestionarioId, Edicao, Nome)
+	VALUES(6, '2019', 'Família');
+
+INSERT INTO ProvaSP.dbo.FatorAssociadoQuestionario(FatorAssociadoQuestionarioId, Edicao, Nome)
+	VALUES(7, '2019', 'Professor');
+
+INSERT INTO ProvaSP.dbo.FatorAssociadoQuestionario(FatorAssociadoQuestionarioId, Edicao, Nome)
+	VALUES(8, '2019', 'Diretor');
