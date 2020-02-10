a8d6383e9b696be569de93055c47184fade09bf1
Marcos A. Schuelter <marcos.schuelter@amcom.com.br>
2020-02-10 09:52:14 -0300

    #11297 Filtros e apresentação de participação com área de conhecimento

--- Src/ProvaSP.Model/Participacao.cs
+++ Src/ProvaSP.Model/Participacao.cs
@@ -7,9 +7,14 @@ namespace ProvaSP.Model.Entidades
         public string Sigla { get; set; }
         public string Titulo { get; set; }
         public string AnoEscolar { get; set; }
-        public int TotalPrevisto { get; set; }
-        public int TotalPresente { get; set; }
-        public float PercentualParticipacao { get; set; }
+        public int TotalPrevistoGeral { get; set; }
+        public int TotalPresenteGeral { get; set; }
+        public float PercentualParticipacaoGeral { get; set; }
+
+        public int AreaConhecimentoID { get; set; }
+        public int TotalPrevistoAreaConhecimento { get; set; }
+        public int TotalPresenteAreaConhecimento { get; set; }
+        public float PercentualParticipacaoAreaConhecimento { get; set; }
         public List<Participacao> Itens { get; set; }
     }
 }
