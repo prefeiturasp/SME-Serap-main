8d826ee69b2b0df7ec112aa770f8353facdce994
Marcos A. Schuelter <marcos.schuelter@amcom.com.br>
2020-04-01 13:44:03 -0300

    #11887 Controle de cache para a string de conexão do banco de dados

--- Src/GestaoAvaliacao.Repository/Context/ConnectionReadOnly.cs
+++ Src/GestaoAvaliacao.Repository/Context/ConnectionReadOnly.cs
@@ -1,18 +1,54 @@
 ﻿using MSTech.Data.Common;
+using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.Caching;
 
 namespace GestaoAvaliacao.Repository.Context
 {
     public class ConnectionReadOnly
     {
+
+        private static readonly MemoryCache _cache = new MemoryCache(typeof(ConnectionReadOnly).FullName);
+
         public IDbConnection Connection
         {
             get
             {
-                TalkDBTransactionCollection collection = new TalkDBTransactionCollection();
-                return new SqlConnection(collection["GestaoAvaliacao"].GetConnection.ConnectionString);
+                TalkDBTransactionCollection talkDBTransaction = GetItem("talkDBTransaction") as TalkDBTransactionCollection;
+                return new SqlConnection(talkDBTransaction["GestaoAvaliacao"].GetConnection.ConnectionString);
             }
         }
+        
+        public static object GetItem(string key)
+        {
+           return AddOrGetExisting(key, () => InitTalkDBTransactionCollection());
+        }
+
+        private static T AddOrGetExisting<T>(string key, Func<T> valueFactory)
+        {
+            var cachePolicy = new CacheItemPolicy()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(10)
+            };
+
+            var newValue = new Lazy<T>(valueFactory);
+            var oldValue = _cache.AddOrGetExisting(key, newValue, cachePolicy) as Lazy<T>;
+            try
+            {
+                return (oldValue ?? newValue).Value;
+            }
+            catch
+            {
+                _cache.Remove(key);
+                throw;
+            }
+        }
+
+        private static object InitTalkDBTransactionCollection()
+        {
+            return new TalkDBTransactionCollection();
+        }
+
     }
 }
