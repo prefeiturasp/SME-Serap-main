33320897bca09687886837e59fa297a4344e52a2
Jdulley Castro de Freitas <jdulley.freitas@amcom.com.br>
2021-01-11 14:22:50 -0300

    Cria;áo d ahome do serap do aluno.

---
+++ Src/GestaoAvaliacao/Assets/js/angular/controllers/electronicTest/homeAlunoElectronicTestController.js
@@ -0,0 +1,27 @@
+﻿(function (angular, $) {
+
+    'use strict';
+
+    //~SETTER
+    angular
+        .module('appMain', ['services', 'filters', 'directives']);
+
+    //~GETTER
+    angular
+        .module('appMain')
+        .controller("HomeAlunoElectronicTestController", HomeAlunoElectronicTestController);
+
+
+    HomeAlunoElectronicTestController.$inject = ['$scope', '$notification', '$location', '$anchorScroll', '$util', '$timeout', '$window'];
+
+
+    function HomeAlunoElectronicTestController(ng, $notification, $location, $anchorScroll, $util, $timeout, $window) {
+
+        function Init() {
+            $notification.clear();
+        };
+
+        Init();
+
+    };
+})(angular, jQuery);
