4826a355798305bd665b8bda66381aa57a7521a0
krisamcom <68442245+krisamcom@users.noreply.github.com>
2021-07-22 22:13:01 -0300

    bug: 43706 - correção

---
+++ Scripts/fix-43706/scripts_correcao.sql
@@ -0,0 +1,7 @@
+
+ insert into GestaoAvaliacao_SGP..ACA_Aluno values (3009305,'YASMIN PAIXAO CAMARGO','6CF424DC-8EC3-E011-9B36-00155D033206','5142051',getdate(),getdate(),1,null,null,'C7B17CC3-72F1-4D15-BACA-0174E58EE041')
+ insert into GestaoAvaliacao_SGP..MTR_MatriculaTurma values (3009305,2,427,417199,137,1,6,1,getdate(),getdate(),30,'2021-02-04',null,6)
+ insert into GestaoAvaliacao_SGP..ACA_Aluno values (3009306,'SAMUEL DE JESUS AMARAL OCANHA','6CF424DC-8EC3-E011-9B36-00155D033206','7655910',getdate(),getdate(),1,null,null,'CD0491C3-5816-4D33-BCCB-19AD90760BB4')
+ insert into GestaoAvaliacao_SGP..MTR_MatriculaTurma values (3009306,1,559,405334,130,1,3,1,getdate(),getdate(),6,'2021-02-04',null,68)
+ insert into CoreSSO..PES_PessoaDeficiencia values ('CD0491C3-5816-4D33-BCCB-19AD90760BB4','A991B05D-A078-41E5-AEF2-A33DF5192481')
+ insert into CoreSSO..PES_PessoaDeficiencia values ('CD0491C3-5816-4D33-BCCB-19AD90760BB4','D41C5E1B-4C69-4301-8BA6-3A152AE975B5')
