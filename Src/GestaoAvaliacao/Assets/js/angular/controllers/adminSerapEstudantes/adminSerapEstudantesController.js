2f926fedb12d20d178409b118e08f5a23b65bdd1
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-01-14 19:35:55 -0300

    feature:56637 - menu acesso admin serap estudantes

---
+++ Src/GestaoAvaliacao/Assets/js/angular/controllers/adminSerapEstudantes/adminSerapEstudantesController.js
@@ -0,0 +1,18 @@
+﻿; (function (angular, $) {
+
+    'use strict';
+
+    //~SETTER
+    angular
+        .module('appMain', ['services', 'filters', 'directives', 'tooltip']);
+
+    //~GETTER
+    angular
+        .module('appMain')
+        .controller("AdminSerapEstudantesController", AdminSerapEstudantesController);
+
+    AdminSerapEstudantesController.$inject = ['$scope'];
+
+    function HomeController(ng) {};
+
+})(angular, jQuery);
---
+++ Src/GestaoAvaliacao/Controllers/AdminSerapEstudantesController.cs
@@ -0,0 +1,33 @@
+﻿using GestaoAvaliacao.Entities.DTO;
+using GestaoAvaliacao.WebProject.Entities;
+using GestaoAvaliacao.WebProject.Facade;
+using MSTech.CoreSSO.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace GestaoAvaliacao.Controllers
+{
+    public class AdminSerapEstudantesController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            if (!SessionFacade.UsuarioLogadoIsValid)
+            {
+                throw new NotImplementedException();
+            };
+
+            string urlApiSerapEstudantes = WebConfigurationManager.AppSettings["URL_ADMIN_SERAP_ESTUDANTES"];
+            string chaveApi = WebConfigurationManager.AppSettings["ChaveSerapProvaApi"];
+            UsuarioLogado user = SessionFacade.UsuarioLogado;
+            string urlAdminEstudantes = $"{urlApiSerapEstudantes}{user.Usuario?.usu_login}/{user.Nome}/{user.Grupo?.gru_id}/{chaveApi}";
+            AdminSerapEstudantesDTO adminSerapEstudantesDTO = new AdminSerapEstudantesDTO();
+            adminSerapEstudantesDTO.UrlApiSerapEstudantes = urlAdminEstudantes;
+            return View(adminSerapEstudantesDTO);
+        }
+    }
+}
