0d802f37ebd1b2ebb1f5a5246c55f7496b40add5
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-06-03 14:50:34 -0300

    story:65641 - criação ItemController

---
+++ Src/GestaoAvaliacao.API/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using GestaoAvaliacao.API.App_Start;
+using GestaoAvaliacao.API.Models;
+using GestaoAvaliacao.Entities;
+using GestaoAvaliacao.IBusiness;
+using GestaoAvaliacao.Util;
+using GestaoAvaliacao.WebProject.Facade;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace GestaoAvaliacao.API.Controllers
+{
+    [CustomAuthorizationAttribute]
+    public class ItemController : ApiController
+    {
+        private readonly IItemBusiness itemBusiness;
+
+        public ItemController(IItemBusiness itemBusiness)
+        {
+            this.itemBusiness = itemBusiness;
+        }
+
+        [Route("api/Item/Save")]
+        [HttpPost]
+        [ResponseType(typeof(ItemResult))]
+        public async Task<HttpResponseMessage> ItemSave([FromBody] ItemModel model)
+        {
+            Item entity = new Item();
+            try
+            {
+
+                //entity = itemBusiness.Save(0, item);
+                var result = new ItemResult();
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                LogFacade.SaveError(ex);
+                var result = new ItemResult();
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+        }
+    }
+}
