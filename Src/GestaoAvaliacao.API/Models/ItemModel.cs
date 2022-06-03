ca420a7acf7b743df40e6471c69a0482aefb6680
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-06-03 18:45:51 -0300

    story:65641 - análise regras de negócio

--- Src/GestaoAvaliacao.API/Models/ItemModel.cs
+++ Src/GestaoAvaliacao.API/Models/ItemModel.cs
@@ -8,5 +8,52 @@
         public int AlternativeOrder { get; set; }
         public bool EmptyAlternative { get; set; }
         public bool DuplicateAlternative { get; set; }
+
+
+        //-------- PASSO 1
+        //disciplina **
+        //modeloMatriz **
+        //matriz **
+
+        //-------- PASSO 2
+        //textobase.Description
+        //textobase.BaseTextOrientation(Fonte)
+
+        //-------- PASSO 3
+        //Código do item
+
+        //Eixo.Id **
+        //Habilidade.Id **
+
+        //assunto.Id **
+        //subassunto.Id **
+
+        //situacaoId(Situação do item)**
+        //tipoItemId(Tipo do item)**
+        //sigiloItem(boleano) **
+
+        //palavrasChave(array de palavras) **
+        //proficiencia (Valor de 100 a 500)
+
+        //series(ANOS ESCOLARES) **
+        //dificuldadeId [ITEMLEVEL](Dificuldade sugerida) **
+
+        //observacao(Observação) **
+
+        //--TRI
+        //Discriminação(0 até 10)
+        //Dificuldade(texto)
+        //Acerto casual(0 até 1)
+
+        //video
+
+        //audio
+
+        //Enunciado(texto) **
+
+        //-- Alternativas (o número de alternatinas tem que ser igual ao tipo do item que informa a quantidade de alternativas **VALIDAR NA HORA DE SALVAR**)
+        //texto **
+        //justificativa
+
     }
 }
