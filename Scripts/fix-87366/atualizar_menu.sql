e16ad3006a0f90187ed9388f6647f38e9b44ddb1
Fernando Groeler <fernando.groeler@gmail.com>
2023-02-23 15:43:48 -0400

    Adicionado script para atualizar a descrição do menu.

---
+++ Scripts/fix-87366/atualizar_menu.sql
@@ -0,0 +1,24 @@
+use CoreSSO
+
+IF EXISTS (SELECT 1 FROM SYS_Modulo sm  WHERE sm.sis_id = 204 and sm.mod_nome = 'Cadastrar novo item teste')  
+BEGIN  
+	UPDATE SYS_Modulo 
+	SET mod_nome = 'Banco de itens (Novo)',
+		mod_descricao = null,
+		mod_dataAlteracao = CURRENT_TIMESTAMP 
+	WHERE sis_id = '204'
+	and mod_nome = 'Cadastrar novo item'
+END  
+ELSE  
+BEGIN  
+	INSERT INTO SYS_Modulo values (204,
+		(select MAX(mod_id) + 1 from SYS_Modulo where sis_id = 204),
+		'Banco de itens (Novo) teste',
+		null,
+		14,
+		0,
+		1,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP
+	)
+END  
