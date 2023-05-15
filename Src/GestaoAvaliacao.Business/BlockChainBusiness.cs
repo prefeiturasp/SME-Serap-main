aa07528421554404ff6e432f50611a01ac8b370a
Caíque Siqueira <caique.siqueira@amcom.com.br>
2023-05-15 14:14:27 -0300

    retirando comentario

--- Src/GestaoAvaliacao.Business/BlockChainBusiness.cs
+++ Src/GestaoAvaliacao.Business/BlockChainBusiness.cs
@@ -25,12 +25,12 @@ namespace GestaoAvaliacao.Business
         {
             valid.Message = null;
 
-            //var duplicateOrder = entity.BlockChainItems?.GroupBy(x => x.Order)
-            //    .Where(g => g.Count() > 1)
-            //    .Select(y => y.Key).ToList();
+            var duplicateOrder = entity.BlockChainItems?.GroupBy(x => x.Order)
+                .Where(g => g.Count() > 1)
+                .Select(y => y.Key).ToList();
 
-            //if (duplicateOrder != null && duplicateOrder.Any())
-            //    valid.Message = "Existe mais de um item com a mesma ordem.";
+            if (duplicateOrder != null && duplicateOrder.Any())
+                valid.Message = "Existe mais de um item com a mesma ordem.";
 
             if (!(entity.Test.UsuId == usuId || (vision == EnumSYS_Visao.Administracao && entity.Test.TestType.Global)))
                 valid.Message = "Apenas o proprietário da prova pode alterá-la";
