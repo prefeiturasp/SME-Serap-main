2906e90f1e8a9ae9d46b76145444d17039e9f1a1
Fernando Groeler <fernando.groeler@gmail.com>
2023-05-24 17:09:33 -0400

    Implementando nova coluna e chamada para o simulador

---
+++ Src/GestaoAvaliacao/Controllers/SimuladorSerapEstudantesController.cs
@@ -0,0 +1,41 @@
+﻿using GestaoAvaliacao.WebProject.Facade;
+using System.Web.Configuration;
+using System;
+using System.Web.Mvc;
+
+namespace GestaoAvaliacao.Controllers
+{
+    public class SimuladorSerapEstudantesController : Controller
+    {
+        [Authorize]
+        public ActionResult Index(long blockId)
+        {
+            try
+            {
+                if (!SessionFacade.UsuarioLogadoIsValid)
+                    throw new NotImplementedException();
+
+                var urlSimuladorSerapEstudantes = WebConfigurationManager.AppSettings["URL_SIMULADOR_SERAP_ESTUDANTES"];
+
+                if (string.IsNullOrWhiteSpace(urlSimuladorSerapEstudantes))
+                    throw new ApplicationException("Necessário configurar a chave 'URL_SIMULADOR_SERAP_ESTUDANTES' no Web.config");
+
+                if (blockId <= 0)
+                    throw new ApplicationException("É necessário informar o ID do caderno.");
+
+                var urlSimulador = $"{urlSimuladorSerapEstudantes}{blockId}";
+
+                return Redirect(urlSimulador);
+            }
+            catch (Exception ex)
+            {
+                LogFacade.SaveError(ex);
+
+                if (ex.InnerException != null)
+                    LogFacade.SaveError(ex.InnerException);
+
+                return RedirectToAction("Index", "Error");
+            }
+        }
+    }
+}
