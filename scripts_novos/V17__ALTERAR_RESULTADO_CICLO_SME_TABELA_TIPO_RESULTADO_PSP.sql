23df6cb1d80aab8a24b77b446c3babe0485d84ce
fernando.groeler <fernando.groeler@gmail.com>
2023-07-19 09:00:07 -0400

    Ajustado script para alteração do tipo do resultado.

---
+++ scripts_novos/V17__ALTERAR_RESULTADO_CICLO_SME_TABELA_TIPO_RESULTADO_PSP.sql
@@ -0,0 +1,8 @@
+use GestaoAvaliacao;
+
+UPDATE dbo.TipoResultadoPsp
+SET Nome = 'Resultado SME - Ciclo de Aprendizagem',
+	State = 1,
+	ModeloArquivo = '"Edicao";"AreaConhecimentoID";"CicloID";"Valor";"TotalAlunos";"NivelProficienciaID";"PercentualAbaixoDoBasico";"PercentualBasico";"PercentualAdequado";"PercentualAvancado";"PercentualAlfabetizado"
+2021;1;1;176,33;106299;"NA";0,41;0,31;0,14;0,04;0,10'
+where Codigo = 11
