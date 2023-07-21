ccbd4610e65ac18c1c0a2e0e8cf6dace79b421ec
fernando.groeler <fernando.groeler@gmail.com>
2023-07-21 14:07:48 -0400

    Ajustado dados para o modelo do arquivo.

--- scripts_novos/V17__ALTERAR_RESULTADO_CICLO_SME_TABELA_TIPO_RESULTADO_PSP.sql
+++ scripts_novos/V17__ALTERAR_RESULTADO_CICLO_SME_TABELA_TIPO_RESULTADO_PSP.sql
@@ -4,5 +4,5 @@ UPDATE dbo.TipoResultadoPsp
 SET Nome = 'Resultado SME - Ciclo de Aprendizagem',
 	State = 1,
 	ModeloArquivo = '"Edicao";"AreaConhecimentoID";"CicloID";"Valor";"TotalAlunos";"NivelProficienciaID";"PercentualAbaixoDoBasico";"PercentualBasico";"PercentualAdequado";"PercentualAvancado";"PercentualAlfabetizado"
-2021;1;1;176,33;106299;"NA";0,41;0,31;0,14;0,04;0,10'
+2021;1;1;176,33;106299;0;0,41;0,31;0,14;0,04;0,10'
 where Codigo = 11
