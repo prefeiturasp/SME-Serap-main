3f53e4e470c5cc6c6fec0be52ead135329b2839d
Caíque Siqueira <caique.siqueira@amcom.com.br>
2023-07-27 14:28:15 -0300

    .

--- scripts_novos/V18__ALTERAR_RESULTADO_CICLO_DRE_TABELA_TIPO_RESULTADO_PSP.sql
+++ scripts_novos/V18__ALTERAR_RESULTADO_CICLO_DRE_TABELA_TIPO_RESULTADO_PSP.sql
@@ -4,5 +4,5 @@ UPDATE dbo.TipoResultadoPsp
 SET Nome = 'Resultado DRE - Ciclo de Aprendizagem',
 	State = 1,
 	ModeloArquivo = '"Edicao";"AreaConhecimentoID";"uad_sigla";"CicloID";"Valor";"TotalAlunos";"NivelProficienciaID";"PercentualAbaixoDoBasico";"PercentualBasico";"PercentualAdequado";"PercentualAvancado";"PercentualAlfabetizado"
-	2021;1;"BT",2;176,332920347322;106299;"NA";0,51;0,31;0,14;0,04;"NA"'
+2021;1;"BT",2;176,332920347322;106299;"NA";0,51;0,31;0,14;0,04;"NA"'
 where Codigo = 8
