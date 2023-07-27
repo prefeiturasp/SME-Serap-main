b3ae0d36d1118ea157a62cbf1844f062fd3307bd
Caíque Siqueira <caique.siqueira@amcom.com.br>
2023-07-27 19:23:43 -0300

    Ajuste modelo script

--- scripts_novos/V18__ALTERAR_RESULTADO_CICLO_DRE_TABELA_TIPO_RESULTADO_PSP.sql
+++ scripts_novos/V18__ALTERAR_RESULTADO_CICLO_DRE_TABELA_TIPO_RESULTADO_PSP.sql
@@ -1,8 +1,9 @@
 use GestaoAvaliacao;
 
+
 UPDATE dbo.TipoResultadoPsp
 SET Nome = 'Resultado DRE - Ciclo de Aprendizagem',
 	State = 1,
 	ModeloArquivo = '"Edicao";"AreaConhecimentoID";"uad_sigla";"CicloID";"Valor";"TotalAlunos";"NivelProficienciaID";"PercentualAbaixoDoBasico";"PercentualBasico";"PercentualAdequado";"PercentualAvancado";"PercentualAlfabetizado"
-2021;1;"BT",2;176,332920347322;106299;"NA";0,51;0,31;0,14;0,04;"NA"'
-where Codigo = 8
+2022;3;"BT";2;209.200;0;0;0.60;0.40;0.10;0.00;1.00;'
+where Codigo = 10
