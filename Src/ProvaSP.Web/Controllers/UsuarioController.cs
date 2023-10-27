47a8d98751f352d7a23cfc0bacceea62242421a5
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2023-10-27 08:20:29 -0300

    História: 105522 Complementações dos questionários de CP e professor, incluíndo novas questões. Adpatação para obter as respostas do usuário. Verificação se há questões do usuário pendentes do questionário para permitir a complementação das respostas.

--- Src/ProvaSP.Web/Controllers/UsuarioController.cs
+++ Src/ProvaSP.Web/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
-﻿using ProvaSP.Data;
+﻿using Newtonsoft.Json;
+using ProvaSP.Data;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -12,8 +14,7 @@ namespace ProvaSP.Web.Controllers
         [HttpGet]
         public HttpResponseMessage RespondeuQuestionario(string edicao, int questionarioID, string usu_id)
         {
-            var retorno = false;
-
+            bool retorno;
             try
             {
                 retorno = DataUsuario.RespondeuQuestionario(edicao, questionarioID, usu_id);
@@ -27,6 +28,26 @@ namespace ProvaSP.Web.Controllers
             var response = Request.CreateResponse(HttpStatusCode.OK);
             response.Content = new StringContent(retorno.ToString(), Encoding.UTF8, "text/plain");
             return response;
-        }       
+        }
+
+        [HttpGet]
+        public HttpResponseMessage RespostasUsuario(int questionarioID, string usu_id)
+        {
+            IEnumerable<RespostasQuestao> retorno;
+
+            try
+            {
+                retorno = DataUsuario.ObterRespostasQuestionarioPorUsuario(questionarioID, usu_id);
+            }
+            catch (Exception ex)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.ExpectationFailed) { ReasonPhrase = ex.Message.Replace(Environment.NewLine, " ") };
+                throw new HttpResponseException(resp);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(retorno), Encoding.UTF8, "application/json");
+            return response;
+        }
     }
 }
