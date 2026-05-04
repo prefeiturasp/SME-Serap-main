883289eaedebad0d9f861486fd4b42bdb27d84db
Mario Junior <mariosj9@gmail.com>
2026-05-04 16:04:56 -0300

    AB#147745

---
+++ Scripts/story-146875/scriptCicloNulavel.sql
@@ -0,0 +1,11 @@
+ALTER TABLE FatorAssociadoQuestionarioRespostaDRE 
+ALTER COLUMN CicloId INT NULL;
+
+ALTER TABLE FatorAssociadoQuestionarioRespostaEscola 
+ALTER COLUMN CicloId INT NULL;
+
+ALTER TABLE FatorAssociadoQuestionarioRespostaSME 
+ALTER COLUMN CicloId INT NULL;
+
+ALTER TABLE Constructo  
+ALTER COLUMN CicloId INT NULL;
